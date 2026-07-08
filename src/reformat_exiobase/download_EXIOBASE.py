7267013423ad961f2003cffa7cdaba108f1325b3
Rita Maestri <rita.maestri95@gmail.com>
2026-07-08 16:40:16 +0200

    fixed positional arguments in download_EXIOBASE

--- src/reformat_exiobase/download_EXIOBASE.py
+++ src/reformat_exiobase/download_EXIOBASE.py
@@ -8,9 +8,9 @@ Created on Tue Mar 18 16:42:38 2025
 import pymrio
 
 #download EXIOBASE
-def download_EXIOBASE(EXIOBASE_folder,system, years, version):
+def download_EXIOBASE(EXIOBASE_folder,system, years, version, overwrite_existing=False):
     print("Downloading EXIOBASE...")
-    pymrio.download_exiobase3(EXIOBASE_folder, system, years, version)
+    pymrio.download_exiobase3(EXIOBASE_folder, system=system, years=years, doi=version, overwrite_existing=overwrite_existing)
     
         
         
